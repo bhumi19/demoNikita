5888c63b37dcaa9a7a6e30963ae2eaaee0023515
Bhumi Gajjar <bhumi.gajjar19@gmail.com>
2015-02-11 17:59:33 +0530

    Initial Commit

---
+++ demoGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demoGit
+//
+//  Created by Sunny on 11/02/15.
+//  Copyright (c) 2015 ample-arch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demoGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demoGit
+//
+//  Created by Sunny on 11/02/15.
+//  Copyright (c) 2015 ample-arch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
